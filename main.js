10447baee668d0bf5f635c805602b1ed99403960
Iris <irisppm@gmail.com>
2020-12-11 17:07:34 -0600

    actualizacion de css y html

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var button= document.getElementById("enviarmensaje");
 var mensaje= document.getElementById("Mensajeenviado");
+var name= document.getElementById("inputname").value;
+
 console.log(button)
+console.log(name)
 button.addEventListener("click",function(){
+    console.log(name)
     mensaje.classList.add("mostrar")
 })
 
